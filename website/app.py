ec5a8d644b86c3ca29bf300caa14fbc240771037
melitacornelio21 <melitacornelio21@gmail.com>
2026-02-21 15:34:55 +0530

    Update app.py

--- website/app.py
+++ website/app.py
@@ -185,6 +185,8 @@ def predict_datapoint():
         return redirect(url_for("home"))
 
 # ================= RUN =================
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
